16428bac9a2c35532260dc5303f865c33ce3d16d
seming2 <popqr3@naver.com>
2025-02-01 01:12:44 +0900

    버튼 컴포넌트 생성

--- src/common/footer/Footer.jsx
+++ src/common/footer/Footer.jsx
@@ -1,9 +1,13 @@
+import Button from '../button';
 import { FooterWrap } from './style';
 
 const Footer = () => {
   return (
     <FooterWrap>
       <div>footer</div>
+      <Button variant='gray' size='medium'>
+        바로가기
+      </Button>
     </FooterWrap>
   );
 };
