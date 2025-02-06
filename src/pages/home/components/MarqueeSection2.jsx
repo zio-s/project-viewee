952900243be16bb64d215bffc419cd6756f17996
name <email address>
2025-02-06 17:03:59 +0900

    swiper layout 수정

---
+++ src/pages/home/components/MarqueeSection2.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import { MarqueeItem, MarqueeWrapper, NewCardsSectionTitle } from '../style';
+
+const MarqueeSection = () => {
+  return (
+    <>
+      <Marquee direction="left" speed={50} gradient={false} loop={0}>
+        <MarqueeWrapper>
+          <MarqueeItem>moive1</MarqueeItem>
+          <MarqueeItem>moive2</MarqueeItem>
+          <MarqueeItem>moive3</MarqueeItem>
+          <MarqueeItem>moive4</MarqueeItem>
+          <MarqueeItem>moive5</MarqueeItem>
+          <MarqueeItem>moive6</MarqueeItem>
+        </MarqueeWrapper>
+      </Marquee>
+    </>
+  );
+};
+
+export default MarqueeSection;
--- src/pages/home/components/SwiperSection.jsx
+++ src/pages/home/components/SwiperSection.jsx
@@ -11,11 +11,28 @@ const SwiperSection = () => {
       slidesPerView={'auto'}
       spacebetween={42}
       coverflowEffect={{
-        rotate: 50,
-        stretch: 300,
-        depth: 500,
+        // rotate: 50,
+        // stretch: 300,
+        // depth: 500,
         modifier: 1,
         slideShadows: false,
+      }}
+      breakpoints={{
+        320: {
+          // slidesPerView: 1,
+          spaceBetween: 20,
+          coverflowEffect: { rotate: 30, stretch: 100, depth: 200 },
+        },
+        600: {
+          // slidesPerView: 1,
+          spaceBetween: 30,
+          coverflowEffect: { rotate: 40, stretch: 200, depth: 300 },
+        },
+        1024: {
+          // slidesPerView: 1,
+          spaceBetween: 42,
+          coverflowEffect: { rotate: 50, stretch: 300, depth: 500 },
+        },
       }}
       pagination={{ clickable: true }}
       modules={[EffectCoverflow, Pagination]}
