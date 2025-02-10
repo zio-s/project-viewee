4feacebb37f1af7437973fffd804a7c707a251dd
jojosg <jojosg93@naver.com>
2025-02-10 15:36:09 +0900

    Feature/home (#38)
    * swiper layout 수정
    
    * 메인페이지 레이아웃 2차 수정 및 순위 이미지 추가
    
    * Home 및 Login 반응형 작업
    
    ---------
    
    Co-authored-by: name <email address>
    Co-authored-by: zio-s <131343462+zio-s@users.noreply.github.com>

--- src/pages/auth/login/style.js
+++ src/pages/auth/login/style.js
@@ -1,28 +1,129 @@
 import styled from 'styled-components';
+import { mobileMore, tabletMore } from '../../../styled/Mixin';
 
 export const Container = styled.div`
+  ${mobileMore`
   display: flex;
+  padding: 20px;
+  `}
   flex-direction: column;
   align-items: center;
-  padding: 20px;
 `;
 
 export const LogoSection = styled.div`
-  margin-bottom: 20px;
+  display: none;
+  ${mobileMore` 
+    display:block;
+   margin-bottom: 45px;
+   margin-top: 70px;
+   font-size: var(--font-content-m);
+  `}
 `;
 
 export const Form = styled.form`
+  ${mobileMore`
+   max-width: 600px;
+ `}
+  margin: 0 auto;
+  max-width: 300px;
   width: 100%;
-  max-width: 600px;
-  background: #1c1c1e;
+  padding: 20px;
+  background: var(--background-color);
   padding: 20px;
   border-radius: 10px;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
 `;
 
 export const InfoBox = styled.div`
-  margin-bottom: 30px;
-  padding: 0px 75px;
+  margin: 0 auto;
+  margin-bottom: 20px;
+  padding: 0px; 30px;
+  ${mobileMore`
+  margin-bottom: 15px;
+  padding: 0px 70px;
+  `}
+`;
+
+export const LoginBox = styled.div`
+  margin-bottom: 10px;
+  margin-top: 30px;
+  ${mobileMore`
+  margin-top: 30px;
+  margin-bottom: 20px;
+  padding: 0px 70px;
+  `}
+`;
+
+export const RequestBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  margin-bottom: 40px;
+  ${mobileMore`
+  padding: 0px 70px;`}
+`;
+
+export const CheckBoxArea = styled.div`
+  display: flex;
+  align-items: center; /* 체크박스와 텍스트 정렬 */
+  color: var(--gray-40);
+  font-size: var(--font-label-m-tablet);
+  ${mobileMore`
+  gap: 12px; /* 간격 조정 */
+  font-size: var(--font-label-xl-mobile);
+  `}
+`;
+
+export const ForgotPass = styled.div`
+  display: flex;
+  align-items: center; /* 체크박스와 텍스트 정렬 */
+  color: var(--gray-40);
+  font-size: var(--font-label-m-tablet);
+  gap: 5px;
+  ${mobileMore`
+  gap: 12px; /* 간격 조정 */
+  font-size: 14px;
+  `};
+`;
+
+export const SimpleLogin = styled.div`
+  &::before,
+  &::after {
+    content: '';
+    flex: 1;
+    border-bottom: 1px solid var(--gray-60);
+    margin: 0 5px;
+  }
+  ${mobileMore`
+  font-size: 17px;
+  padding: 0 70px;
+  `}
+  display: flex;
+  align-items: center;
+  font-size: var(--font-label-xl-mobile);
+  color: var(--gray-40);
+  text-align: center;
+`;
+
+export const SnsButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  margin-top: 20px;
+`;
+
+export const NotMember = styled.div`
+  display: flex;
+  align-items: center; /* 체크박스와 텍스트 정렬 */
+  color: var(--gray-40);
+  font-size: var(--font-label-m-tablet);
+  justify-content: center;
+  gap: 8px;
+  margin-top: 40px;
+  ${mobileMore`
+  gap: 12px; /* 간격 조정 */
+  font-size: 14px;
+  `};
 `;
 
 export const Label = styled.span`
