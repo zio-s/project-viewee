91b2c7f2208d1b64af9f293b6fd8a1d14150438d
seming2 <popqr3@naver.com>
2025-02-11 23:07:16 +0900

    이미지를 백그라운드 상에서 미리 업로드 하여 로딩시간 단축

--- src/pages/category/components/CategoryList.jsx
+++ src/pages/category/components/CategoryList.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { CateGoryBox, CateGoryItem } from './style';
+import { useEffect } from 'react';
 
 const CategoryList = ({ data, category }) => {
   const navigate = useNavigate();
@@ -10,7 +11,20 @@ const CategoryList = ({ data, category }) => {
       state: { type: item.first_air_date ? 'tv' : 'movie' },
     });
   };
-
+  const preloadDetailImages = (items) => {
+    items.forEach((item) => {
+      const path = item.backdrop_path || item.poster_path;
+      if (path) {
+        const img = new Image();
+        img.src = `https://image.tmdb.org/t/p/w1280${path}`;
+      }
+    });
+  };
+  useEffect(() => {
+    if (data.length) {
+      preloadDetailImages(data);
+    }
+  }, [data]);
   return (
     <CateGoryBox>
       {data?.map((item) => (
