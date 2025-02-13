b1065b44f8afbf7a88696aa569fbf2dcd9c188c9
jojosg <jojosg93@naver.com>
2025-02-13 17:09:19 +0900

    Feature/home (#54)
    * swiper layout 수정
    
    * 메인페이지 레이아웃 2차 수정 및 순위 이미지 추가
    
    * Home 및 Login 반응형 작업
    
    * test
    
    * auth 반응형 작업
    
    * swiper 네비게이션 추가
    
    * auth 레이아웃 수정 및 홈페이지 스와이퍼 네비게이션 추가
    
    ---------
    
    Co-authored-by: name <email address>
    Co-authored-by: zio-s <131343462+zio-s@users.noreply.github.com>

--- src/pages/auth/signup/components/SocialSignUp.jsx
+++ src/pages/auth/signup/components/SocialSignUp.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { SnsButtonContainer } from '../style';
+import { SimpleSignup, SnsButtonContainer } from '../style';
 import SnsButton from '../../login/components/SnsButton';
 
 const SocialSignUp = () => (
-  <SnsButtonContainer>
-    <SnsButton type="google" href="#" />
-    <SnsButton type="twitter" href="#" />
-    <SnsButton type="facebook" href="#" />
-  </SnsButtonContainer>
+  <>
+    <SimpleSignup>간편 가입</SimpleSignup>
+    <SnsButtonContainer>
+      <SnsButton type="google" href="#" />
+      <SnsButton type="twitter" href="#" />
+      <SnsButton type="facebook" href="#" />
+    </SnsButtonContainer>
+  </>
 );
 
 export default SocialSignUp;
--- src/pages/home/components/SwiperSection.jsx
+++ src/pages/home/components/SwiperSection.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
-import { StyledSwiper, StyledSwiperSlide, PlayButton } from '../style';
+import { StyledSwiper, StyledSwiperSlide, PlayButton, MainCustomButtonPrev, MainCustomButtonNext } from '../style';
 import Button from '../../../ui/button/defaultButton';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
@@ -8,6 +8,26 @@ import 'swiper/css/pagination';
 
 const SwiperSection = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [isTablet, setIsTablet] = useState(null);
+  const [isMobile, setIsMobile] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsTablet(window.innerWidth <= 1023);
+      setIsMobile(window.innerWidth <= 767);
+    };
+
+    if (typeof window !== 'undefined') {
+      handleResize();
+      window.addEventListener('resize', handleResize);
+    }
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  if (isTablet === null || isMobile === null) return null;
 
   return (
     <StyledSwiper
@@ -34,13 +54,27 @@ const SwiperSection = () => {
           coverflowEffect: { rotate: 50, stretch: 300, depth: 500 },
         },
       }}
-      modules={[EffectCoverflow, Pagination]}
+      navigation={{
+        nextEl: '.new-swiper-button-next',
+        prevEl: '.new-swiper-button-prev',
+        enabled: true,
+      }}
+      modules={[EffectCoverflow, Pagination, Navigation]}
       className="mySwiper"
       onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
     >
       {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num, index) => (
         <StyledSwiperSlide key={num} className={index === activeIndex ? 'swiper-slide-active' : ''}>
-          <img src={`https://swiperjs.com/demos/images/nature-${num}.jpg`} alt={`Nature ${num}`} />
+          <img
+            src={
+              isMobile
+                ? `/images/mobile_mainPoster${num}.png`
+                : isTablet
+                ? `/images/tablet_mainPoster${num}.png`
+                : `/images/mainPoster${num}.png`
+            }
+            alt={`poster ${num}`}
+          />
           {index === activeIndex && (
             <PlayButton>
               <Button size="small">재생하기</Button>
@@ -48,6 +82,12 @@ const SwiperSection = () => {
           )}
         </StyledSwiperSlide>
       ))}
+      <MainCustomButtonPrev className="new-swiper-button-prev">
+        <img src="/icons/arrow/left.svg" alt="" />
+      </MainCustomButtonPrev>
+      <MainCustomButtonNext className="new-swiper-button-next">
+        <img src="public/icons/arrow/right.svg" alt="" />
+      </MainCustomButtonNext>
     </StyledSwiper>
   );
 };
