6a3f5746e0fa52eacc6dc439fe9d0fa3ff1ca8a6
jojosg <jojosg93@naver.com>
2025-02-14 17:37:13 +0900

    Feature/home (#62)
    * swiper layout 수정
    
    * 메인페이지 레이아웃 2차 수정 및 순위 이미지 추가
    
    * Home 및 Login 반응형 작업
    
    * test
    
    * auth 반응형 작업
    
    * swiper 네비게이션 추가
    
    * auth 레이아웃 수정 및 홈페이지 스와이퍼 네비게이션 추가
    
    * SpecialSection 추가 및 홈 레이아웃 수정
    
    ---------
    
    Co-authored-by: name <email address>
    Co-authored-by: zio-s <131343462+zio-s@users.noreply.github.com>

--- src/pages/home/components/MarqueeSection.jsx
+++ src/pages/home/components/MarqueeSection.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
-import { MarqueeItem, MarqueeWrapper, NewCardsSectionTitle } from '../style';
+import { MarqueeItem, MarqueeWrapper } from '../style';
 
 const MarqueeSection = () => {
   return (
     <>
-
-      <NewCardsSectionTitle>지구에서 나만 안본 그 작품</NewCardsSectionTitle>
-
       <Marquee direction="left" speed={50} gradient={false} loop={0}>
         <MarqueeWrapper>
           <MarqueeItem>moive1</MarqueeItem>
