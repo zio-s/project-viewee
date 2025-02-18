f98716d5a3f243ca76ea3e8ae5b36fbde2a38234
zio-s <131343462+zio-s@users.noreply.github.com>
2025-02-18 16:28:16 +0900

    Feature/landing (#72)
    * landing page 구현중
    
    * 랜딩페이지 3d gsap 효과 구현 (미완성)
    카드 스티키 효과 구현중
    
    * 랜딩페이지 제작중 ( gsapSlice.js를 생성하여 중앙 관리 )

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,16 +1,42 @@
-import CardSection from './components/CardSection';
+import { useEffect, useRef } from 'react';
+import Lenis from '@studio-freight/lenis';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import CardSection from './components/card/CardSection';
 import Hero from './components/Hero';
-import ReviewSection from './components/ReviewSection';
 import ScrollSection from './components/ScrollSection';
 import { LandingWrap } from './style';
 
 const LandingPage = () => {
+  const lenisRef = useRef();
+
+  useEffect(() => {
+    lenisRef.current = new Lenis({
+      duration: 2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      direction: 'vertical',
+      smooth: true,
+      smoothTouch: false,
+      touchMultiplier: 2,
+    });
+    lenisRef.current.on('scroll', ScrollTrigger.update);
+    gsap.ticker.add((time) => {
+      lenisRef.current.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+    return () => {
+      if (lenisRef.current) {
+        lenisRef.current.destroy();
+        gsap.ticker.remove(lenisRef.current.raf);
+      }
+    };
+  }, []);
+
   return (
     <LandingWrap>
       <Hero />
       <CardSection />
-      {/* <ReviewSection /> */}
-      {/* <ScrollSection /> */}
+      <ScrollSection />
     </LandingWrap>
   );
 };
