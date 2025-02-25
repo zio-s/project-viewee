bd3b9e609c9f24b7f7dd09ab1bdc301742e94535
dayeonback <dbaeg275@gmail.com>
2025-02-25 11:54:17 +0900

    Feature/detail (#120)
    * detail page
    
    * herosection 생성
    episode, episodelist 로 수정
    recommended, recommendedmovies로 수정
    
    * InfoSection 상세정보
    
    * 상세정보 api 정보 완료
    
    * 시리즈/회차 있고 없음를 구분하여 네브 바 변경 구현중
    
    * 시리즈 api 연동 완료
    
    * 등급 정보 api 연동 완료
    +이미지화 필요
    
    * 시리즈 정보 있을 때 에피소드탭 뜨도록 수정
    
    * 에피소드 정보 유무에 따른 네브 탭 상태 변경중
    
    * nav tab 구현 완료
    에피소드 정보 유무에 따른 탭 변환과 엑티브
    
    * overview onclick 시 상세정보로 이동
    overview 95자 이상일시 '...더보기' 적용
    
    * nav api 경로 수정
    
    * 미선 승기 세민님 작업 풀
    
    * Merge branch 'develop' into feature/detail
    
    * HeroSection 스타일 90% 완료
    
    * 히어로 섹션 스타일 수정중
    
    * 카멜케이스 수정
    
    * HeroSection 스타일 수정 완료
    
    * Episode 스타일 수정
    
    * recmmendedmovis 작업완료
    
    * detail 페이지
    
    * hero 탭바 엑티브 수정
    
    * hero 탭바 엑티브 수정
    
    * 등급 사진화 완료
    전체적인 세부 스타일 수정
    
    * episodelist selectedSeason 수정중
    
    * 리뷰섹션 작업중
    
    * 에피소드리스트 tv버전 수정중
    
    * 리뷰 등록 구현
    
    * 리뷰섹션 작업중
    
    * 리뷰섹션 기능 구현 완료
    
    * 리뷰 섹션 등록 및삭제 구현
    어스에 연결해야됌
    
    * 디테일 수정
    
    * 리뷰섹션 수정/삭제 기능 추가
    에피소드리스트 스와이퍼
    
    * 레커먼드 영화 클릭시 해당 영화로 이동
    
    * 레커먼드 섹션 시밀러로 변경
    
    ---------
    
    Co-authored-by: dayeonback <dbaeg257@gmail.com>
    Co-authored-by: zio-s <131343462+zio-s@users.noreply.github.com>

--- src/pages/detail/components/RecommendedMovies.jsx
+++ src/pages/detail/components/RecommendedMovies.jsx
@@ -2,8 +2,10 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 import { RecommendeddBox } from '../style';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const RecommendedMovies = () => {
+  const navigate = useNavigate();
   const { detail, loading, error } = useSelector((state) => state.detailR);
 
   if (loading) return <div>로딩 중...</div>;
@@ -12,28 +14,50 @@ const RecommendedMovies = () => {
 
   const recommendedList = detail.recommendations.results;
 
+  const handleMovieClick = (movie) => {
+    let category = "movie"; // 기본값: 영화
+
+    if (movie.type === "드라마") category = "drama";
+    else if (movie.type === "예능") category = "comedy";
+    else if (movie.type === "애니") category = "animation";
+    else if (movie.type === "키즈") category = "kids";
+
+    navigate(`/${category}/${movie.id}`);
+  };
+
   return (
     <>
-    <RecommendeddBox>
-      <h2>추천 영화</h2>
-      <Swiper spaceBetween={10} slidesPerView={'auto'}>
-        {recommendedList.map((movie) => (
-          <SwiperSlide key={movie.id} className="movie-slide">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </RecommendeddBox>
-    <RecommendeddBox>
-      <h2>연관 작품</h2>
-      <Swiper spaceBetween={10} slidesPerView={'auto'}>
-        {recommendedList.map((movie) => (
-          <SwiperSlide key={movie.id} className="movie-slide">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </RecommendeddBox>
+      <RecommendeddBox>
+        <h2>추천 영화</h2>
+        <Swiper spaceBetween={10} slidesPerView={'auto'}>
+          {recommendedList.map((movie) => (
+            <SwiperSlide key={movie.id} className="movie-slide">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                onClick={() => handleMovieClick(movie)}
+                style={{ cursor: 'pointer' }}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </RecommendeddBox>
+
+      <RecommendeddBox>
+  <h2>연관 작품</h2>
+  <Swiper spaceBetween={10} slidesPerView={'auto'}>
+    {detail.similar?.results?.map((movie) => (
+      <SwiperSlide key={movie.id} className="movie-slide">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+          onClick={() => handleMovieClick(movie)}
+          style={{ cursor: 'pointer' }}
+        />
+      </SwiperSlide>
+    ))}
+  </Swiper>
+</RecommendeddBox>
     </>
   );
 };
