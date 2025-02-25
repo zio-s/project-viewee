311e0d8da2c5fc8094e484517b10974c7ac4b18e
msun <114346839+msun97@users.noreply.github.com>
2025-02-25 10:37:53 +0900

    Feature/mypage (#115)
    * 마이페이지/나의 컨텐츠 페이지 작업
    
    * 마이페이지 tab 구현
    
    * 삭제 구현
    
    * 프로필 전환/수정 구현 및 작성 리뷰 작업 진행중
    
    * review 기능 구현
    
    * mypage 수정
    
    * 프로필 전환 및 요금제 반응형 구현
    
    * 고객센터 공지사항 페이지 구현
    
    * 로그인 구현 및 카카오 로그인 API 구현중
    
    * 회원가입 / 문의 내역 기능 추가
    
    * 회원가입 오류 수정 / 고객센터 페이지 수정
    
    * 질문 사이트 수정
    
    * 문의 페이지 수정 / 고객센터 페이지 완료
    
    * 마이너 버그 / 수정사항
    
    * 마이너 버그 수정
    
    * 마이너 수정
    
    * 마이너 버그 수정
    
    * 마이너 수정
    
    * 마이너 수정
    
    * modifyProfile 구현
    
    ---------
    
    Co-authored-by: msun97 <msun9_7@naver.com>

--- src/pages/auth/modifyprofile/components/InputField.jsx
+++ src/pages/auth/modifyprofile/components/InputField.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Input from '../../../../ui/input';
 
-const InputField = ({ label, placeholder = '' }) => (
+const InputField = ({ name, value, type, label, placeholder = '', onChange }) => (
   <div className="info_box">
     <span className="label">{label}</span>
     <div className="input_box">
-      <Input variant="gray" size="medium" fullWidth={true} placeholder={placeholder} />
+      <Input
+        variant="gray"
+        size="medium"
+        fullWidth={true}
+        placeholder={placeholder}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   </div>
 );
